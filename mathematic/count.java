a2cfaaf9b0123589d8cc02e826bbfee560ffd7c0
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-02 23:39:24 +0530

    timecomplexity is theta(d) or O(d)

--- mathematic/count.java
+++ mathematic/count.java
@@ -12,12 +12,18 @@ public class count{
         return count;
     }
     */
+    // recursive approch
+    /*
+                    timecomplexity is theta(d) "because if we give d number digit then it's count will d "
+    */
     public static int countDigit(int n){
         
         if(n==0)
           return 0;
           return 1 + countDigit(n/10);
     }
+    
+   
     public static void main(String args[]) {
     int num = 789;
    System.out.println(countDigit(num));
