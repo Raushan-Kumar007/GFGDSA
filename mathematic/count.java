10c02869aea626867ca7a284334be07b1a588989
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-02 23:16:40 +0530

    number of count using recursive approach

--- mathematic/count.java
+++ mathematic/count.java
@@ -1,7 +1,9 @@
 package mathematic;
 
 public class count{
-     public static int countDigit(int n){
+     
+    /*        iterative approch
+    public static int countDigit(int n){
         int count = 0;
         while(n!=0){
             n = n/10;
@@ -9,7 +11,13 @@ public class count{
         }
         return count;
     }
-     
+    */
+    public static int countDigit(int n){
+        
+        if(n==0)
+          return 0;
+          return 1 + countDigit(n/10);
+    }
     public static void main(String args[]) {
     int num = 789;
    System.out.println(countDigit(num));
