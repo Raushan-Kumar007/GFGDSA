41530a566c96458bdd56f499668453c0fb7ba57e
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-03 00:50:03 +0530

    checking is number palindrom or not

---
+++ mathematic/isPalendrom.java
@@ -0,0 +1,21 @@
+package mathematic;
+
+public class isPalendrom {
+    public static boolean isPelNum(int n){
+        int rev = 0;
+        int temp = n;
+        while (temp !=0) {
+            int lastdigit = temp % 10; // remainder
+            rev = (rev*10)+lastdigit;
+            temp = temp/10; //quotent
+        }
+        return (rev==n);
+    }
+    public static void main(String[] args) {
+        int num = 122;
+        if (isPelNum(num)==true)
+        System.out.println("yes : it's palindrom");
+        else
+        System.out.println("No : it's not a palindrom");
+    }
+}
