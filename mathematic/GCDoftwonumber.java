508771e640ed6404866550348dd4dbd058c10d1b
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-04 16:54:47 +0530

    finding GCD of two number using  Eucledean algorithm

--- mathematic/GCDoftwonumber.java
+++ mathematic/GCDoftwonumber.java
@@ -1,7 +1,8 @@
 package mathematic;
 
 public class GCDoftwonumber {
-    public static  int GCD(int a, int b){
+    // time complexity = O(min(a,b))
+   /* public static  int GCD(int a, int b){
         int res = Math.min(a, b);
         while(res>0){
             if(a%res==0 && b%res==0){
@@ -11,9 +12,20 @@ public class GCDoftwonumber {
         }
         return res;
     }
+    */
+    // Eucledean Algorithm (let 'b' be smaller than 'a' GCD(a,b)=GCD(a-b, b))
+    public static  int GCD(int a, int b){
+        while(a!=b){
+            if(a>b)
+            a = a-b;
+            else
+            b = b - a;
+        }
+        return a;
+    }
     public static void main(String[] args) {
-        int a = 100;
-        int b = 200;
+        int a = 4;
+        int b = 6;
         System.out.println(GCD(a, b));
         
     }
