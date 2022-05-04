6c720b5c761dd48dfae01b9cb96a0395c6337d53
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-04 17:53:12 +0530

    efficient solution finding of LCM

--- mathematic/LCMoftwonumber.java
+++ mathematic/LCMoftwonumber.java
@@ -1,7 +1,7 @@
 package mathematic;
 
 public class LCMoftwonumber {
-    public static int LCM(int a, int b){
+   /* public static int LCM(int a, int b){
         int res = Math.max(a, b);
         while(true){
             if(res%a==0 && res%b==0)
@@ -9,9 +9,22 @@ public class LCMoftwonumber {
             res++;
         } 
     }
+    */
+    //Efficient Solution 
+    /*
+    using a * b = gcd(a,b)*lcm(a,b)
+    */
+    public static int gcd(int a, int b){
+        if(b==0)
+        return a;
+        return gcd(b, a%b);
+    }
+    public static int LCM(int a, int b){
+        return (a*b)/gcd(a, b);
+    }
     public static void main(String[] args) {
-        int a = 4;
-        int b = 6;
+        int a = 40;
+        int b = 60;
         System.out.println(LCM(a, b));
     }
 }
