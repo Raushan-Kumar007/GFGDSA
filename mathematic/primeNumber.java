dee6b863f16be1dae38629064d22fb3e0e728766
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-04 20:05:52 +0530

    find number of prime using more efficient solution

--- mathematic/primeNumber.java
+++ mathematic/primeNumber.java
@@ -14,7 +14,7 @@ public class primeNumber {
     }
     */
     // efficient solution with time complexity is O(n^1/2)
-    public static boolean primeNo(int n){
+   /* public static boolean primeNo(int n){
         if(n==1 || n==0)
         return false;
         for (int i = 2; i*i < n; i++) {
@@ -22,7 +22,23 @@ public class primeNumber {
            return false; 
         }
         return true;
+    }*/
+    //more efficient solution with time complexity is O(n^1/2)
+    public static boolean primeNo(int n){
+        if(n==1 || n==0)
+        return false;
+        if(n==1||n==3)
+        return true;
+        if(n%2==0 || n%3==0)
+        return false;
+        for(int i=5; i*i<=n; i= i+6){
+            if(n%i==0||n%(i+2)==0)
+            return false;
+            
+        }
+        return true;
     }
+
     public static void main(String[] args) {
         int num = 100;
         for(int i=0; i<=num; i++){
