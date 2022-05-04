9eb8771e9b95b45c093ca3ac5142daa769ad09be
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-04 19:45:15 +0530

    efficient solution to find the prime number with bigO(n^1/2)

--- mathematic/primeNumber.java
+++ mathematic/primeNumber.java
@@ -1,10 +1,23 @@
 package mathematic;
 
 public class primeNumber {
+    /*
+    // naive solution with time complexity is O(n)
     public static boolean primeNo(int n){
         if(n==1 || n==0)
         return false;
         for (int i = 2; i < n; i++) {
+           if(n%i==0)
+           return false; 
+        }
+        return true;
+    }
+    */
+    // efficient solution with time complexity is O(n^1/2)
+    public static boolean primeNo(int n){
+        if(n==1 || n==0)
+        return false;
+        for (int i = 2; i*i < n; i++) {
            if(n%i==0)
            return false; 
         }
