2f917e7854838d8e6410747f2bd0ec3038ceb63e
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-06 19:00:16 +0530

    efficient solution with time complexity is theta(n^1/2)

--- mathematic/AllDevisorOfNum.java
+++ mathematic/AllDevisorOfNum.java
@@ -2,10 +2,21 @@ package mathematic;
 
 public class AllDevisorOfNum {
     // naive solution with time complexity is theta(n) and space complexity is theta(1)
-    static void alldivOfNum(int n){
+   /* static void alldivOfNum(int n){
         for(int i=1; i<=n; i++){
             if(n%i==0){
                 System.out.println(i);
+            }
+        }
+    }*/
+    // efficient solution with time complexity is theta(n^1/2)
+    static void alldivOfNum(int n){
+        for(int i=1; i*i<=n; i++){
+            if(n%i==0){
+                System.out.println(i);
+                if(i!=n/i){
+                    System.out.println(n/i);
+                }
             }
         }
     }
