99c9d36fab3c097212c8247cd5005c9cbf3b1a97
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-06 13:42:18 +0530

    correction in if statement in line number 30

--- mathematic/primeNumber.java
+++ mathematic/primeNumber.java
@@ -14,7 +14,7 @@ public class primeNumber {
     }
     */
     // efficient solution with time complexity is O(n^1/2)
-    public static boolean primeNo(int n){
+   /* public static boolean primeNo(int n){
         if(n==1 || n==0)
         return false;
         for (int i = 2; i*i <= n; i++) {
@@ -22,22 +22,22 @@ public class primeNumber {
            return false; 
         }
         return true;
-    }
+    }*/
     //more efficient solution with time complexity is O(n^1/2)
-    // public static boolean primeNo(int n){
-    //     if(n==1 || n==0)
-    //     return false;
-    //     if(n==1||n==3)
-    //     return true;
-    //     if(n%2==0 || n%3==0)
-    //     return false;
-    //     for(int i=5; i*i<=n; i= i+6){
-    //         if(n%i==0||n%(i+2)==0)
-    //         return false;
+    public static boolean primeNo(int n){
+        if(n==1 || n==0)
+        return false;
+        if(n==2||n==3)
+        return true;
+        if(n%2==0 || n%3==0)
+        return false;
+        for(int i=5; i*i<=n; i= i+6){
+            if(n%i==0||n%(i+2)==0)
+            return false;
             
-    //     }
-    //     return true;
-    // }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         int num = 100;
