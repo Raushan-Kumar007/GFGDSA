cee31a76487f847c7516b1ef5a8e60c3d08f0f2d
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-06 19:15:55 +0530

    more efficient solution to find devisor of a number with time complexity is theta(n^1/2) and auxulary space is theta(1)

--- mathematic/AllDevisorOfNum.java
+++ mathematic/AllDevisorOfNum.java
@@ -10,7 +10,7 @@ public class AllDevisorOfNum {
         }
     }*/
     // efficient solution with time complexity is theta(n^1/2)
-    static void alldivOfNum(int n){
+   /* static void alldivOfNum(int n){
         for(int i=1; i*i<=n; i++){
             if(n%i==0){
                 System.out.println(i);
@@ -19,7 +19,21 @@ public class AllDevisorOfNum {
                 }
             }
         }
-    }
+    }*/
+    // more efficient solution to find devisor of a number with time complexity is theta(n^1/2) and auxulary space is theta(1)
+    static void alldivOfNum(int n){
+        int i;
+        for( i=1; i*i<n; i++){
+            if(n%i==0){
+                System.out.print(i+" ");
+            }
+        }
+            for ( ;i>=1; i--) {
+                if(n%i==0){
+                    System.out.print(n/i+" ");
+                }
+            }
+        }
     public static void main(String[] args) {
         int n =120;
         alldivOfNum(n);
