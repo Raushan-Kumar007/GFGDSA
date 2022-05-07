87dc1bb640ab558ff029f8cb7e2ff213513d55e6
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-07 13:29:30 +0530

    efficient solution with timecomplexity is thete(logn) and spacecomplexity is theta(logn)

--- mathematic/power.java
+++ mathematic/power.java
@@ -1,12 +1,23 @@
 package mathematic;
 
 public class power {
-    static int Power(int x, int n){
+  /*  static int Power(int x, int n){
         int res = 1;
         for(int i=0; i<n; i++){
             res = res * x;
         }
         return res;
+    }*/
+   // efficient solution with timecomplexity is thete(logn) and spacecomplexity is theta(logn)
+    static int Power(int x, int n){
+        if(n==0)
+        return 1;
+        int temp = Power(x,n/2);
+        temp = temp*temp;
+        if(n%2==0)
+        return temp;
+        else
+        return temp*x;
     }
     public static void main(String[] args) {
         
