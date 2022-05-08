c251e2233039ed949abe9e777c1ec0110540c8c0
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-08 12:41:45 +0530

    finding kth bits using right shift operator

--- BitMagic/Kthbit.java
+++ BitMagic/Kthbit.java
@@ -1,15 +1,24 @@
 package BitMagic;
 
 public class Kthbit {
-   static void kthbit(int n, int k){
+    // by left shift 
+  /* static void kthbit(int n, int k){
         if(( n & (1<<(k-1))) != 0){
           System.out.println("Yes");
         }else{
             System.out.println("No");
         }
-    }
+    }*/
+    // by right shift
+    static void isKthbit(int n, int k){
+        if(((n>>(k-1)) & 1)==1){
+         System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    } 
     public static void main(String[] args) {
         
-       kthbit( 5, 3);
+       isKthbit( 5, 3);
     }
 }
