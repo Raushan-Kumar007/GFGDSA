65961de2fc897e62ffd8350af1d627ffbc65e571
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-08 14:10:10 +0530

    number of count by Brian and Kerningham algorithm

--- BitMagic/countSetbit.java
+++ BitMagic/countSetbit.java
@@ -1,13 +1,23 @@
 package BitMagic;
 
 public class countSetbit {
-    static int countSet(int n){
+    // naive solution with time complexity is theta(total bit in n)
+   /* static int countSet(int n){
         int count = 0;
         while(n!=0){
-            if(n%2!=0)
+            if(n%2!=0) // or also using bitwise operator if(n&1==1)
             count++;
-            n = n/2;
+            n = n/2;// or also using bitwise operator n = n>>2
             
+        }
+        return count;
+    }*/
+    //Brian and Kerningham Algorithm time complexity is theta(number of count)
+    static int countSet(int n){
+        int count = 0;
+        while(n>0){
+            n=(n&(n-1));
+            count++;
         }
         return count;
     }
