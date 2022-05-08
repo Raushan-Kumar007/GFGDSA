7fecfd5a63f1a55e6727f6c89edd3ca646339a1f
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-08 16:16:48 +0530

    efficient way to find the power of 2 by brian and kerningham algorithm

--- BitMagic/powerOFtwo.java
+++ BitMagic/powerOFtwo.java
@@ -2,7 +2,7 @@ package BitMagic;
 
 public class powerOFtwo {
     // simple naive solution for finding the power of 2
-    static boolean powerof2(int n){
+   /* static boolean powerof2(int n){
         if(n==0)
         return false;
         while (n != 1) {
@@ -12,9 +12,16 @@ public class powerOFtwo {
         }
         return true;
     }
+    */
+    // efficient way to find the power of 2 by brian and kerningham algorithm 
+    static boolean powerof2(int n){
+        if(n==0)
+        return false;
+        return  ((n&(n-1))==0);
+    }
 
    public static void main(String[] args) {
-       int n = 16;
+       int n = 18;
        if(powerof2(n)==true)
        System.out.println("YES");
        else
