924367182c7082c86133185a93d6cf2f2add8470
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-11 14:20:15 +0530

    efficient way to find number of odd count with time complexity is O(N) and Aux Space is O(1)

--- BitMagic/oneODD.java
+++ BitMagic/oneODD.java
@@ -15,7 +15,7 @@ public class oneODD {
          return 0;
     }
     */
-    
+    // efficient way to find number of odd count with time complexity is O(N) and Aux Space is O(1)
     static int oneoddOccuring(int[] arr, int n){
         int res = 0;
         for(int i=0; i<n; i++){
