f45bf6fe5254852d13b8cef3d57f0a7e902c3b38
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-11 15:06:39 +0530

    finding first bit of a number

--- BitMagic/firstSetBit.java
+++ BitMagic/firstSetBit.java
@@ -1,13 +1,28 @@
 package BitMagic;
 
 public class  firstSetBit{
-    static int setBit(int n){
+    // first way 
+   /* static int setBit(int n){
         if(n==0)
         return 0;
         return (int)(Math.log(n&-n)/Math.log(2.0)) + 1;
 
+    }*/
+    // second way 
+    static int getSetBit(int n){
+        if(n==0)
+        return 0;
+        int count = 0;
+        while(n!=0){
+            count++;
+            if(n%2==1){
+               return count;
+            }
+             n = n>>1; // right sift by 1
+        }
+        return count;
     }
       public static void main(String[] args) {
-          System.out.println(setBit(18)); // 10010==>18
+          System.out.println(getSetBit(0)); // 10010==>18
       }
 }
