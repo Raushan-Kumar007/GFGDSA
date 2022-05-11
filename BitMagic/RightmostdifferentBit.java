9b2b09475e26f85f5ca8d45b62c31e17558dfd6b
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-11 15:27:20 +0530

    finding rightmost diffrent  bit of two number

--- BitMagic/RightmostdifferentBit.java
+++ BitMagic/RightmostdifferentBit.java
@@ -1,7 +1,7 @@
 package BitMagic;
 
 public class RightmostdifferentBit {
-    static int getfirstbit(int n){
+  /*  static int getfirstbit(int n){
         if(n==0)
         return 0;
         return (int)(Math.log(n &-n)/Math.log(2.0))+1;
@@ -11,6 +11,26 @@ public class RightmostdifferentBit {
         return -1;
         return  getfirstbit(m^n);
         
+    }
+    */
+    // Secoond way to find
+    static int getfirstbit(int n){
+        if(n==0)
+        return 0;
+        int count = 0;
+        while(n!=0){
+            count++;
+            if((n%2)==1){
+                return count;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+    static int findrightmostbitdiff(int m, int n){
+        if(m==n)
+        return -1;
+        return getfirstbit(m^n);
     }
     public static void main(String[] args) {
        System.out.println(findrightmostbitdiff(11, 9)); 
