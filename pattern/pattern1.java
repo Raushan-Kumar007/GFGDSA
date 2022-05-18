e9f4d90f4ffe5a311b208261e8c53b77fe38d2d1
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-18 17:16:21 +0530

    print pattern1

--- pattern/pattern1.java
+++ pattern/pattern1.java
@@ -4,11 +4,26 @@ import java.util.Scanner;
 
 // function for print pattern
 public class pattern1 {
-    static void printP1(int n){
+   /*  USING FOR LOOPS
+   static void printP1(int n){
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
                 System.out.print("*");
             }
+            System.out.println();
+        }
+    }
+    */
+    //USING WHILE LOOPS
+    static void printP1(int n){
+        int i =1;
+        while(i<=n){
+            int j=1;
+            while(j<=n){
+                System.out.print("*");
+                j++;
+            }
+            i++;
             System.out.println();
         }
     }
