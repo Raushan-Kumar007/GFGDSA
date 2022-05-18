f66ea428cc8b74051b0f054c90242bb7791138ae
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-18 16:07:50 +0530

    print pattern1

---
+++ pattern/pattern1.java
@@ -0,0 +1,20 @@
+package pattern;
+
+import java.util.Scanner;
+
+// function for print pattern
+public class pattern1 {
+    static void printP1(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in); // user input  
+        int n = Scan.nextInt();
+        printP1(n);
+    }
+}
