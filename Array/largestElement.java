0d9d33ba0b64c4d2f76e26e75b9f99fe42509dbf
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-22 22:53:09 +0530

    efficient approch to find largest element in array

--- Array/largestElement.java
+++ Array/largestElement.java
@@ -1,6 +1,8 @@
 package Array;
 
 public class largestElement {
+
+    /*   Naive Approch to find largest element in array with time Complexity is theta(n^2)
     static int largeElem(int arr[], int n){
         for(int i=0; i<n; i++){
             boolean flag = true;
@@ -16,6 +18,16 @@ public class largestElement {
 
          }
          return -1;
+        }
+        */
+        // efficient approch to find largest element in array with time complexity is theta(n)
+        static int largeElem(int arr[],int n){
+            int res = 0;
+            for(int i=1; i<n; i++){
+                if(arr[i]>arr[res])
+                res = i;
+            }
+            return res;
         }
     public static void main(String[] args) {
         int arr[] = {5,27,19,3,4};
