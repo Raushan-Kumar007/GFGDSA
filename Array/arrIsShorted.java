f4f152e52c1e1189933efd1b7ccf29188dfec059
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-23 12:34:41 +0530

    efficient approch to know array is shorted or not

--- Array/arrIsShorted.java
+++ Array/arrIsShorted.java
@@ -2,7 +2,7 @@ package Array;
 
 public class arrIsShorted {
     // Naive approch with time Complexity is theta(n^2)
-    static boolean isShorted(int[] arr){
+   /* static boolean isShorted(int[] arr){
         int n=arr.length;
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
@@ -10,6 +10,15 @@ public class arrIsShorted {
                 return false;
             }
             
+        }
+        return true;
+    }*/
+    // efficient approch with time complexity is theta(n)
+    static boolean isShorted(int[] arr){
+        int n = arr.length;
+        for(int i=1; i< n; i++){
+            if(arr[i]<arr[i-1])
+            return false;
         }
         return true;
     }
