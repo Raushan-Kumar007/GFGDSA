b75e0f54764c72c1616d09b504ab437a4f512bf7
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-23 13:19:10 +0530

    efficient approch to remove duplicates element from array

--- Array/removeDuplicates.java
+++ Array/removeDuplicates.java
@@ -4,7 +4,7 @@ public class removeDuplicates {
     // naive approch
     // timecomplexity is BigO(n)
     // Auxullary space is BigO(n)
-    static int removeDup(int arr[], int n){
+  /*  static int removeDup(int arr[], int n){
         int[] temp = new int[n];
         temp[0]=arr[0]; 
         int res = 1;
@@ -16,6 +16,18 @@ public class removeDuplicates {
         }
         for(int i=0; i<res; i++){
             arr[i] = temp[i];
+        }
+        return res;
+    }*/
+    // Time Complexity is Bigo(N)
+    // Auxullary space is BigO(1)
+    static int removeDup(int arr[], int n){
+        int res = 1;
+        for(int i=1; i<n; i++){
+            if(arr[i]!=arr[res-1]){
+            arr[res] = arr[i];
+            res++;
+            }
         }
         return res;
     }
