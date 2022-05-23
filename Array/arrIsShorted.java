ddfd07de602ef0b1b16702be24f75efec0ea7795
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-23 12:26:33 +0530

    naive approch to know is array is shorted or not either in increasing pr decreasing order

---
+++ Array/arrIsShorted.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class arrIsShorted {
+    // Naive approch with time Complexity is theta(n^2)
+    static boolean isShorted(int[] arr){
+        int n=arr.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[j]<arr[i])
+                return false;
+            }
+            
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,20,30,10};
+        System.out.println(isShorted(arr));
+        int arr1[] = {5,7,10,15,30};
+        System.out.println(isShorted(arr1));
+    }
+}
