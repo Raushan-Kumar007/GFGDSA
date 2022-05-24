d7e60ee14f76a869cc02939a943521706d412564
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-24 18:57:28 +0530

    method111 to left rotate of an array

--- Array/leftrotateArr11.java
+++ Array/leftrotateArr11.java
@@ -20,7 +20,7 @@ public class leftrotateArr11 {
          Auxullary space is theta(d)
 
     */
-    static void leftrotate(int[] arr, int n, int d){
+    /*static void leftrotate(int[] arr, int n, int d){
         int[] temp = new int[d];
         for(int i=0; i<d; i++){
              temp[i]=arr[i];
@@ -31,6 +31,23 @@ public class leftrotateArr11 {
        for(int i=0; i<d; i++){
         arr[n-d+i]=temp[i];
        }
+    }*/
+    /*
+        Auxullary Space = theta(1)
+    */
+    static void reverseArr(int[] arr, int low, int high){
+        while(low<high){
+            int temp = arr[low];
+            arr[low]=arr[high];
+            arr[high]=temp;
+            low++;
+            high--;
+        }
+    }
+    static void  leftrotate(int[] arr, int n, int d){
+        reverseArr(arr, 0, d-1);   // timeComplexity ===> theta(d + n - d + n)
+        reverseArr(arr, d, n-1);       //   timeComplexity ===> theta(2n)
+        reverseArr(arr, 0, n-1);  //  timeComplexity ===> theta(n)
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5}; int n=5; int d = 2;
