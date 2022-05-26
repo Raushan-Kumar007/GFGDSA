d2f11f19736b73d5be3239298ca79d2c103aec4d
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-26 15:10:18 +0530

    efficient approch to find max diff in array

--- Array/MaxArrDiff.java
+++ Array/MaxArrDiff.java
@@ -2,13 +2,24 @@ package Array;
 
 public class MaxArrDiff {
     // naive approch to find max diff in an array
-    static int Maxdiff(int[] arr){
+   /* static int Maxdiff(int[] arr){
         int n = arr.length;
         int res = arr[1]-arr[0];
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 res = Math.max(res,arr[j]-arr[i]);
             }
+        }
+        return res;
+    }*/
+    // efficient approch to find max diff in an array
+    static int Maxdiff(int[] arr){
+        int n = arr.length;
+        int res = arr[1]-arr[0];
+        int minVlu = arr[0];
+        for(int i=0; i<n; i++){
+            res = Math.max(res,arr[i]-minVlu);
+            minVlu = Math.min(minVlu,arr[i]);
         }
         return res;
     }
