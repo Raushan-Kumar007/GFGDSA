72407234bf940ee5da57be91fa91c76300f4f87f
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-26 18:56:33 +0530

    efficient approch to stock buy or sell

--- Array/stockBuyOrSell.java
+++ Array/stockBuyOrSell.java
@@ -1,7 +1,8 @@
 package Array;
 
 public class stockBuyOrSell {
-    static int maxProfit(int[] price,int start, int end){
+    //Naive approch
+   /* static int maxProfit(int[] price,int start, int end){
 
         if(end<=start)
             return 0;
@@ -15,9 +16,19 @@ public class stockBuyOrSell {
          }
         }
         return profit;
+    }*/
+    static int  maxProfit(int[] price, int n){
+        int profit = 0;
+     for(int i=1;i<n; i++){
+         if(price[i]>price[i-1]){
+             profit = profit + (price[i]-price[i-1]);
+
+         }
+     }
+     return profit;
     }
     public static void main(String[] args) {
         int[] arr = {1,5,3,8,12}; int n = 5;
-        System.out.println(maxProfit(arr, 0, n-1));
+        System.out.println(maxProfit(arr,n));
     }
 }
