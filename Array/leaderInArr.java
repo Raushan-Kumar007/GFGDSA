11ea982b8114f7b7ce3281b47e7bd662a4118654
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-26 14:13:27 +0530

    efficient way to find leader of an array

--- Array/leaderInArr.java
+++ Array/leaderInArr.java
@@ -1,8 +1,14 @@
+/*
+    leader of an array  means it's left or right side not greater element than this element
+
+*/
+
+
 package Array;
 
 public class leaderInArr {
-    // naive approch with timeComplexity is theta(n^1/2)
-    static void leaderArr(int[] arr, int n){
+    // naive approch with timeComplexity is theta(n^2)
+    /*static void leaderArr(int[] arr, int n){
         for(int i=0; i<n; i++){
             boolean flag = false;
             for(int j=i+1; j<n; j++){
@@ -15,9 +21,20 @@ public class leaderInArr {
                  System.out.print(arr[i]+" ");
         }
          
+    }*/
+    // efficient approch to find leader of an array
+    static void leaderArr(int[] arr, int n){
+        int currentLeader = arr[n-1];
+        System.out.print(currentLeader+" ");
+        for(int i=n-2; i>=0; i--){
+            if(currentLeader < arr[i]){
+                currentLeader=arr[i];
+                System.out.print(currentLeader+" ");
+            }
+        }
     }
     public static void main(String[] args) {
-        int arr[] = {7,10,4,10,6,5,2};int n=6;
+        int arr[] = {7, 10, 4, 10, 6, 5, 2}, n = 7;
         leaderArr(arr, n);
         
     }
