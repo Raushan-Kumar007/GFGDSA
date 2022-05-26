a9b134fb679c98e2179b427f09aaac83a1b056f7
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-26 20:17:29 +0530

    efficient approch ti find trap rain water with timecomplexity is theta(n)

--- Array/trapRainWater.java
+++ Array/trapRainWater.java
@@ -5,7 +5,7 @@ package Array;
 */
 
 public class trapRainWater {
-    static int getWater(int[] arr, int n){
+  /*  static int getWater(int[] arr, int n){
         int res = 0;
         for(int i=1; i<n-1; i++){
             
@@ -21,6 +21,23 @@ public class trapRainWater {
            
            
             res = res + (Math.min(Lmax,Rmax)-arr[i]);
+        }
+        return res;
+    }*/
+
+    // efficient approch timeComplexity is Theta(n)
+    static int getWater(int[] arr, int n){
+        int res = 0;
+        int[] Lmax = new int[n];
+        int[] Rmax = new int[n];
+        Lmax[0]=arr[0];
+        for(int i=1; i<n; i++)
+        Lmax[i] = Math.max(arr[i],Lmax[i-1]);
+        Rmax[n-1]=arr[n-1];
+        for(int i=n-2; i>=0; i--)
+        Rmax[i]=Math.max(arr[i],Rmax[i+1]);
+        for(int i=1; i<n-1; i++){
+            res = res + (Math.min(Lmax[i],Rmax[i])-arr[i]);
         }
         return res;
     }
