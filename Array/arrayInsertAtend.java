fc7f9aa9389a0717d69090917e1d61304713f1f0
Rausha Kumar <ravisankaranand199@gmail.com>
2022-05-30 16:49:45 +0530

    inserting an element at last in an array

---
+++ Array/arrayInsertAtend.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class arrayInsertAtend {
+    static void arrInsert(int[] arr, int n, int element){
+        for(int i=0;i<n; i++){
+            if(arr[i]==arr[n-1])
+            arr[n-1]=element;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        System.out.println("Before Insertion");
+        for(int i=0;i<arr.length-1; i++){
+          System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        int element = 40;
+        int n = 4;
+        arrInsert(arr, n, element);
+        System.out.println("After Insertion");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
