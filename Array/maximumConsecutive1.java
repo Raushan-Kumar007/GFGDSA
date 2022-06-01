4317ade79bedc8e3558149553ea595802bbd9662
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-02 01:19:33 +0530

    maximum consecutive array with efficient approch

--- Array/maximumConsecutive1.java
+++ Array/maximumConsecutive1.java
@@ -2,7 +2,7 @@ package Array;
 
 public class maximumConsecutive1 {
     // naive approch  with timecomplexity is O(n^2) and Aux space is theta(1)
-    static int maxConsOne(int arr[]){
+   /* static int maxConsOne(int arr[]){
         int res =0;
         int n = arr.length;
         for(int i=0; i<n; i++){
@@ -13,6 +13,20 @@ public class maximumConsecutive1 {
                 else
                 break;
             }
+            res = Math.max(count,res);
+        }
+        return res;
+    }*/
+    // efficient approch with timecomplexity is theta(n) and spaceComplexity is theta(1)
+    static int maxConsOne(int arr[]){
+        int n = arr.length;
+        int res =0;
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i]==0)
+            count = 0;
+            else
+            count++;
             res = Math.max(count,res);
         }
         return res;
