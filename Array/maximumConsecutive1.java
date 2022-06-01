bdf3986ebc57fe2567bef1341cb69809ad377044
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-02 01:13:10 +0530

    Maximum consecutive 1's in an array using naive approch

---
+++ Array/maximumConsecutive1.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class maximumConsecutive1 {
+    // naive approch  with timecomplexity is O(n^2) and Aux space is theta(1)
+    static int maxConsOne(int arr[]){
+        int res =0;
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            int count = 0;
+            for(int j = i; j<n; j++){
+                if(arr[j]==1)
+                count++;
+                else
+                break;
+            }
+            res = Math.max(count,res);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int arr[] ={0,1,1,0,1,0};
+        int n = 6; 
+        System.out.print(maxConsOne(arr));
+    }
+}
