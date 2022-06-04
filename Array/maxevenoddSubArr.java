2879f391ebc41e2e5c325f8906a65744f71338b4
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-04 19:37:52 +0530

    efficient approch to find even odd maximum  subarray

--- Array/maxevenoddSubArr.java
+++ Array/maxevenoddSubArr.java
@@ -2,7 +2,7 @@ package Array;
 
 public class maxevenoddSubArr {
     // naive approch with time complexity is bigO(n^2)
-    static int EvenOddSubArr(int arr[]){
+   /*  static int EvenOddSubArr(int arr[]){
         int n = arr.length;
         int res = 1;
         for(int i=0;i<n;i++){
@@ -14,6 +14,23 @@ public class maxevenoddSubArr {
               break;
             }
             res = Math.max(res,curr);
+        }
+        return res;
+    }*/
+
+    //efficient approch with time complexity is theta(n) and aux space is theta(1)
+
+    static int EvenOddSubArr(int arr[]){
+        int n = arr.length;
+        int res = 1;
+        int curr = 1;
+        for(int i=1; i<n; i++){
+            if(arr[i]%2==0&&arr[i-1]%2!=0 || arr[i]%2!=0&&arr[i-1]%2==0){
+                curr++;
+                res = Math.max(res,curr);
+            }else{
+                curr = 1;
+            }
         }
         return res;
     }
