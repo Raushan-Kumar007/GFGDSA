5a1830fc8177c80a15243654d9cdd68a3946ce23
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-05 12:31:20 +0530

    Efficient approch to find max circular sum

--- Array/MaxSumCircularArr.java
+++ Array/MaxSumCircularArr.java
@@ -2,7 +2,7 @@ package Array;
 
 public class MaxSumCircularArr {
     // naive approch with time Complexity is BigO(n^2)
-    static int maxCircularSumArr(int arr[]){
+   /*  static int maxCircularSumArr(int arr[]){
         int n = arr.length;
         int res = arr[0];
         for(int i=1; i<n; i++){
@@ -16,9 +16,34 @@ public class MaxSumCircularArr {
             res = Math.max(res, Curr_Max);
         }
         return res;
+    }*/
+
+    // efficient Approch  timecomplexity is theta(n)
+    static int NormalMaxSum(int arr[]){  /* Standard kadanes Algorithm */
+        int n = arr.length;
+        int res = arr[0];
+        int maxEnd = arr[0];
+        for(int i=1; i<n; i++){
+            maxEnd = Math.max(arr[i], maxEnd+arr[i]);
+            res = Math.max(res, maxEnd);
+        }
+        return res;
+    }
+    static int overallSum(int arr[]){
+        int n = arr.length;
+        int max_normal = NormalMaxSum(arr);  /*Normal Sum*/
+        if(max_normal<0)
+        return max_normal;
+        int arr_sum = 0;          /*Circular Sum*/
+        for(int i=0; i<n; i++){
+            arr_sum += arr[i];
+            arr[i] = -arr[i];
+        }
+        int max_Circular = arr_sum + NormalMaxSum(arr);
+        return Math.max(max_normal,max_Circular);
     }
     public static void main(String[] args) {
         int arr[] = {5,-2,3,4};
-        System.out.print(maxCircularSumArr(arr));
+        System.out.print(overallSum(arr));
     }
 }
