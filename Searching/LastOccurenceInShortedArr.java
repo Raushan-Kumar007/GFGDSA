c103447f0a7dcb9e508867e470ab54a2322403b7
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-10 12:09:20 +0530

    recursive approch to find last occurence of shorted array

--- Searching/LastOccurenceInShortedArr.java
+++ Searching/LastOccurenceInShortedArr.java
@@ -12,7 +12,7 @@ public class LastOccurenceInShortedArr {
     }
     */
     // iterative approch to find last occurence in shoreted array with log(n) time complexity
-    static int lastOcuurence(int arr[], int x){
+    /*static int lastOcuurence(int arr[], int x){
         int n = arr.length;
         int left = 0;
         int right = n-1;
@@ -31,10 +31,31 @@ public class LastOccurenceInShortedArr {
         }
         return -1;
 
+    }*/
+
+    // recursive approch to find lastOccurence of element in array
+
+    static int lastOcuurence(int arr[], int x, int left, int right){
+        int n = arr.length;
+        if(left>right)
+        return -1;
+        int mid = left + (right-left)/2;
+        if(arr[mid]<x)
+        return lastOcuurence(arr, x, mid+1, right);
+        else if(arr[mid]>x)
+            return lastOcuurence(arr, x, left, mid-1);
+        else{
+            if(mid==n-1||arr[mid]!=arr[mid+1])
+            return mid;
+            else
+            return lastOcuurence(arr, x, mid+1, right);
+      
+        }
     }
     public static void main(String[] args) {
         int arr[] = {10,20,20,30,30,40,40,40};
         int x = 40;
-        System.out.print(lastOcuurence(arr, x));
+        int n = 8;
+        System.out.print(lastOcuurence(arr, x, 0, n-1));
     }
 }
