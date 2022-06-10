4b07b8b1cf6737a2deef64b4b47c4852112b22f0
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-10 11:37:56 +0530

    recursive approch to find first occurence in array

--- Searching/FirstOccurenceInShortedArr.java
+++ Searching/FirstOccurenceInShortedArr.java
@@ -12,7 +12,7 @@ public class FirstOccurenceInShortedArr {
     } 
     */
     // efficient approch to find first occurence in array using Iterarive approch
-    static int firstOccurence(int arr[], int x){
+    /*static int firstOccurence(int arr[], int x){
         int n = arr.length;
         int left = 0;
         int right = n-1;
@@ -31,9 +31,29 @@ public class FirstOccurenceInShortedArr {
         }
         return -1;
     }
+    */
+
+    //recursive approch to find first occurence in an array
+
+    static int firstOccurence(int arr[], int x , int left, int right){
+        if(left>right)
+        return -1;
+        int mid = left + (right - left)/2;
+        if(arr[mid]<x)
+        return firstOccurence(arr, x,mid+1, right);
+        else if(arr[mid]>x)
+        return firstOccurence(arr, x, left, mid-1);
+        else{
+            if(mid==0 || arr[mid-1] != arr[mid])
+            return mid;
+            else
+            return firstOccurence(arr, x, left,mid-1);
+        } 
+    }
     public static void main(String[] args) {
         int arr[] = {1,10,10,10,20,20,40};
+        int n = 7;
         int x = 20;
-        System.out.print(firstOccurence(arr, x));
+        System.out.print(firstOccurence(arr, x,0,n-1));
     }
 }
