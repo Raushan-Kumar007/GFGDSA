93ae12dfa2f460a04fd13cf5b51f0b187f15e930
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-10 11:16:12 +0530

    iterarive approch to find first occurence in an array

--- Searching/FirstOccurenceInShortedArr.java
+++ Searching/FirstOccurenceInShortedArr.java
@@ -2,7 +2,7 @@ package Searching;
 
 public class FirstOccurenceInShortedArr {
     // naive approch with timecomplexity is BigO(n)
-    static int firstOccurence(int arr[], int x){
+   /*  static int firstOccurence(int arr[], int x){
         int n = arr.length;
         for(int i=0;i<n; i++){
             if(arr[i]==x)
@@ -10,6 +10,27 @@ public class FirstOccurenceInShortedArr {
         }
         return -1;
     } 
+    */
+    // efficient approch to find first occurence in array using Iterarive approch
+    static int firstOccurence(int arr[], int x){
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+        while(left<=right){
+            int mid = left + (right - left)/2;
+           if(arr[mid]<x)
+           left = mid + 1;
+           else if(x < arr[mid])
+           right = mid - 1;
+           else{
+               if(mid==0||arr[mid-1]!=arr[mid])
+               return mid;
+               else 
+               right = mid - 1;
+           }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         int arr[] = {1,10,10,10,20,20,40};
         int x = 20;
