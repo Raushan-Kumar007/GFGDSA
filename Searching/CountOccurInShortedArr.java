b0e2f0e6a4a1fac28991d275c121ff0a7c17d92b
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-10 12:46:55 +0530

    Count occurence in shorted array in log(n)time complexity

--- Searching/CountOccurInShortedArr.java
+++ Searching/CountOccurInShortedArr.java
@@ -1,8 +1,10 @@
 package Searching;
 
+import Array.removeDuplicates;
+
 public class CountOccurInShortedArr {
     // Naive approch to find countOccurence in shorted array using bigO(n) time complexity
-    static int countOccurInArr(int arr[], int x){
+   /* static int countOccurInArr(int arr[], int x){
         int n = arr.length;
         int count = 0;
         for(int i=0; i<n; i++){
@@ -11,10 +13,78 @@ public class CountOccurInShortedArr {
            }
         }
         return count;
-    }
+    }*/
+    static int firstOcc(int arr[], int n, int x)
+	{
+		int low = 0, high = n - 1;
+
+		while(low <= high)
+		{
+			int mid = (low + high) / 2;
+
+			if(x > arr[mid])
+				low = mid + 1;
+
+			else if(x < arr[mid])
+				high = mid - 1;
+
+			else
+			{
+				if(mid == 0 || arr[mid - 1] != arr[mid])
+					return mid;
+
+				else
+					high = mid - 1;
+			}
+
+		}
+
+		return -1;
+	}
+
+	static int lastOcc(int arr[], int n, int x)
+	{
+		int low = 0, high = n - 1;
+
+		while(low <= high)
+		{
+			int mid = (low + high) / 2;
+
+			if(x > arr[mid])
+				low = mid + 1;
+
+			else if(x < arr[mid])
+				high = mid - 1;
+
+			else
+			{
+				if(mid == n - 1 || arr[mid + 1] != arr[mid])
+					return mid;
+
+				else
+					low = mid + 1;
+			}
+
+		}
+
+		return -1;
+	}
+	
+	
+	static int countOcc(int arr[], int n, int x)
+	{
+		int first = firstOcc(arr, n, x);
+
+		if(first == -1)
+			return 0;
+		else 
+			return lastOcc(arr, n, x) - first + 1;
+	}
+
   public static void main(String[] args) {
      int arr[] = {10,20,20,20,30,30}; 
      int x = 20;
-     System.out.print(countOccurInArr(arr, x));
+     int n =6;
+     System.out.println(countOcc(arr, n, x));
   }  
 }
