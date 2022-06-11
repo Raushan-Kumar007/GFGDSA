009ac261cf805240275e91c499780c94d012a2dd
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-11 13:17:36 +0530

    efficient approch to find square root of a number using binary search

--- Searching/SquareRoot.java
+++ Searching/SquareRoot.java
@@ -2,16 +2,35 @@ package Searching;
 
 public class SquareRoot {
     // naive approch 
-    static int SquareRoots(int n){
+    /*static int SquareRoots(int n){
         int i =1;
          while (i*i<=n) {
             i++;
          }
          return (i-1);    
+    }*/
+    static int SquareRoots(int x){
+        int left = 1;
+        int right =x;
+        int res =-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            int mSqr = mid*mid;
+            if(mSqr==x)
+            return mid;
+            else if(mSqr>x)
+            right=mid-1;
+            else{
+                left=mid+1;
+                res = mid;
+            }
+            
+        }
+        return res;
+
     }
     public static void main(String[] args) {
-        int n = 10;
-        System.out.println(SquareRoots(n));
+        System.out.println(SquareRoots(10));
     }
     
 }
