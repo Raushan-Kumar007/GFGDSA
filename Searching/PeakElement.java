37c95a9d91984c5400da5328935c16d843c3d300
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-11 17:51:42 +0530

    efficient approch to find peak element in array

--- Searching/PeakElement.java
+++ Searching/PeakElement.java
@@ -4,7 +4,7 @@ public class PeakElement {
 
     // Naive approch to find peak element
 
-    static int peak(int arr[]){
+   /*  static int peak(int arr[]){
         int n = arr.length;
        if(n==1)
        return arr[0];
@@ -14,9 +14,24 @@ public class PeakElement {
        return arr[n-1];
        for(int i=1; i<n-1; i++){
         if(arr[i]>=arr[i-1]&&arr[i]>=arr[i+1])
-        return arr[i];
+        return i;
        } 
        return -1;
+    }*/
+
+     static int peak(int arr[]){
+        int n = arr.length;
+        int left = 0; int right = n-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if((mid==0||arr[mid-1]<=arr[mid])&&(mid==n-1||arr[mid+1]<=arr[mid]))
+            return mid;
+            if(mid>0 && arr[mid-1]>=arr[mid])
+            right = mid-1;
+            else
+            left = mid + 1;
+        }
+        return -1;
     }
     public static void main(String[] args) {
         int arr[] = {5, 10, 11, 12, 20, 12};
