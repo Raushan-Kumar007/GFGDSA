42e70932c3f0d0eb9b09b3cca95ebc02bdaab990
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-22 13:37:06 +0530

    repeating element

---
+++ Searching/repeatElement2.java
@@ -0,0 +1,24 @@
+package Searching;
+
+public class repeatElement2 {
+    static int findRepeating(int arr[]){
+        int n = arr.length;
+        int slow = arr[0],fast=arr[0];
+        do{
+            slow=arr[slow];
+            fast=arr[arr[fast]];
+        }
+        while(slow != fast);
+        slow =arr[0];
+        while(slow != fast)
+        {
+            slow = arr[slow];
+            fast = arr[fast];
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 2, 4, 6, 5, 7, 3}, n= 8;
+        System.out.println(findRepeating(arr)); 
+    }
+}
