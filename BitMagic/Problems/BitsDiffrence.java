1c1e319e768e0af88ff7b711fd9c6deb06860877
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-23 01:45:26 +0530

    Bit Difference

---
+++ BitMagic/Problems/BitsDiffrence.java
@@ -0,0 +1,49 @@
+/*You are given two numbers A and B. The task is to count the number of bits needed to be flipped to convert A to B.
+
+Example 1:
+
+Input: A = 10, B = 20
+Output: 4
+Explanation:
+A  = 01010
+B  = 10100
+As we can see, the bits of A that need 
+to be flipped are 01010. If we flip 
+these bits, we get 10100, which is B.
+Example 2:
+
+Input: A = 20, B = 25
+Output: 3
+Explanation:
+A  = 10100
+B  = 11001
+As we can see, the bits of A that need 
+to be flipped are 10100. If we flip 
+these bits, we get 11001, which is B.
+
+Your Task: The task is to complete the function countBitsFlip() that takes A and B as parameters and returns the count of the number of bits to be flipped to convert A to B.
+
+Expected Time Complexity: O(log N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ A, B ≤ 106 */
+
+
+
+package BitMagic.Problems;
+
+public class BitsDiffrence {
+    static int countBit(int n){
+        int count = 0;
+        while(n!=0){
+            if((n&1)==1)
+                count++;
+            n=n>>1;
+        }
+        return count;
+    }
+    public static int countBitsFlip(int a, int b){
+        return countBit(a^b);
+    }
+}
