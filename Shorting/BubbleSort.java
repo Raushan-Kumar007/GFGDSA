ea42aba223074d20a0644c5ad950b3b9e08a729f
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-23 19:07:32 +0530

    Naive approch for Bubble sort

---
+++ Shorting/BubbleSort.java
@@ -0,0 +1,26 @@
+package Shorting;
+
+public class BubbleSort {
+
+    // Naive Approch 
+
+    static void Bubble(int arr[]){
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                     arr[j] = arr[j+1];
+                     arr[j+1] = temp;
+                }
+            }
+        }
+    }
+     public static void main(String[] args) {
+        int arr[] = {12,6,18,3};
+        Bubble(arr);
+        for(int i=0;i<arr.length;i++){
+         System.out.print(arr[i]+" ");
+        }
+     }
+}
