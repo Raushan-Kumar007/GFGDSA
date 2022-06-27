8bc64a621143d1ab54d131b1cee3ddf5fca8d1ca
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-27 11:25:13 +0530

    efficient approch for mearge sort

--- Shorting/MergeSort.java
+++ Shorting/MergeSort.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MergeSort {
-    static void Merge(int arr1[], int arr2[]){
+
+    // Naive approch with theta(m+n)theta(log(m+n))
+
+   /*  static void Merge(int arr1[], int arr2[]){
         int n = arr1.length;
         int m = arr2.length;
         int newArr[] = new int[m+n];
@@ -19,29 +22,39 @@ public class MergeSort {
         System.out.print(newArr[i]+" ");
        }
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr1[] = new int[n];
-        System.out.println("Enter array1");
-        for(int i=0;i<n;i++){
-            arr1[i]=sc.nextInt();
-        }
-        for(int i=0;i<n;i++){
+    */
+
+    // efficient approch timecomplexity is theta(m+n) Auxullary space is bigO(1)
+
+    static void Merge(int arr1[],int arr2[]){
+        int n = arr1.length;
+        int m = arr2.length;
+         int i=0; int j = 0;
+         while(i<n && j<m){
+         if(arr1[i]<=arr2[j]){
             System.out.print(arr1[i]+" ");
+            i++;
+         }else{
+            System.out.print(arr2[j]+" ");
+            j++;
+         }
         }
-        System.out.println();
-        System.out.println("Enter array2");
-        int m = sc.nextInt();
-        int arr2[] = new int[m];
-        for(int i=0;i<m;i++){
-            arr2[i]=sc.nextInt();
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(arr2[i]+" ");
-        }
-        System.out.println();
-        System.out.println("new array");
+         while(i<n){
+            System.out.print(arr1[i]+" ");
+            i++;
+         }
+         while(j<m){
+            System.out.print(arr2[j]+" ");
+            j++;
+         }
+    }
+    public static void main(String[] args) {
+        int arr1[]={10,20,45,90};
+        int arr2[]={1,3,25,40,65};
+        int arr3[]=new int[arr1.length+arr2.length];
         Merge(arr1, arr2);
+        for(int i=0;i<arr1.length+arr2.length;i++){
+            System.out.print(arr3[i]+" ");
+        }
     }
 }
