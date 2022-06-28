04d84b3f8c6997456e6d24990722ce66134aa423
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-28 13:31:16 +0530

    Union of two arrays

--- Shorting/UnionOfTwoSortedArr.java
+++ Shorting/UnionOfTwoSortedArr.java
@@ -1,9 +1,8 @@
 package Shorting;
 
-import java.util.Arrays;
 
 public class UnionOfTwoSortedArr {
-    static void Union(int arr1[], int arr2[]){
+   /*  static void Union(int arr1[], int arr2[]){
         int n = arr1.length;
         int m = arr2.length;
        int arr3[] = new int[n+m];
@@ -20,13 +19,30 @@ public class UnionOfTwoSortedArr {
         }
        }
     }
-    public static void main(String[] args) {
-        int arr1[] = {3,5,8};
-        int arr2[] = {2,8,9,10,15};
-        int arr3[] = new int[arr1.length+arr2.length];
-        Union(arr1, arr2);
-        for(int i=0;i<arr3.length;i++){
-            System.out.print(arr3[i]+" ");
+    */
+    public static void main (String[] args) 
+    {
+        int a[] = new int[]{3,8,8};
+        int b[] = new int[]{2,8,8,10,15};
+        
+        int m = a.length;
+        int n = b.length;
+	    printUnion(a,b,m,n);
+        
+    }
+    
+    static void printUnion(int a[], int b[], int m, int n){
+    
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(i>0 && a[i-1]==a[i]){i++;continue;}
+            if(j>0 && b[j-1]==b[j]){j++;continue;}
+            if(a[i]<b[j]){System.out.print(a[i]+" ");i++;}
+            else if(a[i]>b[j]){System.out.print(b[j]+" ");j++;}
+            else{System.out.print(a[i]+" ");i++;j++;}
         }
+            while(i<m){if(i==0||a[i]!=a[i-1])System.out.print(a[i]+" ");i++;}
+            while(j<n){if(j==0||b[j]!=b[j-1])System.out.print(b[j]+" ");j++;}
     }
 }
+
