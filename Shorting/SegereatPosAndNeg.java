c660052c9ed93ffcf2c36cf072295c0c7e2aea5e
Rausha Kumar <ravisankaranand199@gmail.com>
2022-07-11 16:11:16 +0530

    efficient approch for sorting + & - element in arrays

--- Shorting/SegereatPosAndNeg.java
+++ Shorting/SegereatPosAndNeg.java
@@ -4,7 +4,7 @@ public class SegereatPosAndNeg {
 
     // Naive approch with time complexity is theta(n) and Auxullary space is theta(n)
 
-    static void sortPosAndNegative(int arr[]){
+   /*  static void sortPosAndNegative(int arr[]){
         int n = arr.length;
         int temp[] = new int[n];
         int i = 0;
@@ -22,9 +22,30 @@ public class SegereatPosAndNeg {
             arr[j]=temp[j];
         }
     }
+    */
+
+    // Hores patition is using in efficient approch
+
+    static void sortPosAndNegative(int arr[], int n){
+     
+    int i = -1; int j = n;
+      while(true){
+        do{
+           i++; 
+        }while(arr[i]<0);
+        do{
+            j--;
+        }while(arr[j]>=0);
+        if(i>=j)
+            return;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+      }
+    }
     public static void main(String[] args) {
        int arr[] = {13,-12,18,-10};
-       sortPosAndNegative(arr);
+       sortPosAndNegative(arr,4);
        for(int i=0;i<arr.length;i++){
         System.out.print(arr[i]+" ");
        } 
