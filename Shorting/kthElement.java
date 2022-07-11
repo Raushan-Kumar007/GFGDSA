830e88ab25bcec30e275407c69e79b6746e03ad5
Rausha Kumar <ravisankaranand199@gmail.com>
2022-07-11 13:08:41 +0530

    Efficient Approch for Kth element

--- Shorting/kthElement.java
+++ Shorting/kthElement.java
@@ -4,7 +4,7 @@ public class kthElement {
 
     // Naive Approch for finding Kth element
 
-    static void swap(int arr[],int i, int j){
+ /*    static void swap(int arr[],int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
@@ -23,9 +23,47 @@ public class kthElement {
         SelectionSort(arr);
         return arr[k-1];
     }
+    */
+    static int partition(int arr[], int l, int h)
+    {   
+        int pivot=arr[h];
+        int i=l-1;
+        for(int j=l;j<=h-1;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[h];
+        arr[h]=temp;
+        return i+1;
+    }
+    
+    static int kthSmallest(int arr[],int n,int k){
+        int l=0,r=n-1;
+        while(l<=r){
+            int p=partition(arr,l,r);
+            if(p==k-1)
+                return p;
+            else if(p>k-1)
+                r=p-1;
+            else
+                l=p+1;
+        }
+        return -1;
+    }
+    
+
     public static void main(String[] args) {
-        int arr[] = new int[]{10,5,30,12};
-       System.out.print(Kth(arr, 2));
+        int arr[] = new int[]{10,4,5,8,11,6,26};
+        
+        int n = arr.length;int k=5;
+        int index=kthSmallest(arr,n,k);
+        
+        System.out.print(arr[index]);
          
     }
 }
