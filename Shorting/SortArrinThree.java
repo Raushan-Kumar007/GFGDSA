6bcd30f8af5f59f51074bd543926043603849f83
Rausha Kumar <ravisankaranand199@gmail.com>
2022-07-11 17:13:46 +0530

    sorts 0s,1s,2s

--- Shorting/SortArrinThree.java
+++ Shorting/SortArrinThree.java
@@ -1,8 +1,10 @@
 package Shorting;
 
+import java.util.Arrays;
+
 public class SortArrinThree {
     // Naive Approch with BigO(n) time complexity and theta(n) Auxullary Space
-    static void sort0s1s2s(int arr[]){
+ /*    static void sort0s1s2s(int arr[]){
         int n = arr.length;
         int temp[] = new int[n];
         int i = 0;
@@ -25,9 +27,36 @@ public class SortArrinThree {
             arr[j]=temp[j];
         }
     }
+    */
+    static void sort(int arr[],int n){
+        int l=0,h=n-1,mid=0;
+        while(mid<=h){
+            switch(arr[mid]){
+                case 0:
+                    //swapping arr[l] & arr[mid])
+                    int temp=arr[l];
+                    arr[l]=arr[mid];
+                    arr[mid]=temp;
+                    
+                    l++;mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    //swapping arr[h] & arr[mid])
+                    temp=arr[h];
+                    arr[h]=arr[mid];
+                    arr[mid]=temp;
+                    
+                    h--;
+                    break;
+            }
+        }
+     }
    public static void main(String[] args) {
-       int arr[] = {0,0,1,2,1,1,0,2,1};
-       sort0s1s2s(arr);
+       int arr[] = {0,0,1,2,1,1,0,2,1};int n=9;
+       sort(arr,n);
        for(int i=0;i<arr.length;i++){
         System.out.print(arr[i]+" ");
        }
