986cfc022d25b6e41af194c87a9a56198583ba7f
Rausha Kumar <ravisankaranand199@gmail.com>
2022-08-08 01:34:07 +0530

    two dimension arrays with argument type

---
+++ matrix/TwoDArrArgument.java
@@ -0,0 +1,26 @@
+package matrix;
+
+import java.util.Scanner;
+
+public class TwoDArrArgument {
+    static void twoDarray(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int arr[][] = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        twoDarray(arr);
+    }
+}
